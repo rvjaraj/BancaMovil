debed6dd0aa6abaf62b9172184ef389e02669a60
Ricardo Vinicio Jara jara <5940607-RicardoVinicioJara@users.noreply.gitlab.com>
2020-06-02 19:19:31 -0500

    TrabajadorDAO

---
+++ src/main/java/ec/edu/ups/proycto/dao/TrabajadorDAO.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.proycto.dao;
+
+import ec.edu.ups.proyecto.emtitis.Trabajador;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Ricardo
+ */
+@Stateless
+public class TrabajadorDAO {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public TrabajadorDAO() {
+    }
+    
+    
+    public void insert(Trabajador trabajador) throws Exception {
+        try {
+            em.persist(trabajador);
+        } catch (Exception e) {
+            throw new Exception("Erro ingreso Trabajador " + e.getMessage());
+        }
+    }
+
+    public void delete(Trabajador trabajador) throws Exception {
+        try {
+            em.remove(read(trabajador.getId()));
+        } catch (Exception e) {
+            throw new Exception("Erro Eliminar Trabajador " +e.getMessage());
+        }
+    }
+
+    public void deleteId(int id) throws Exception {
+        try {
+            System.out.println("borrando");
+            em.remove(read(id));
+        } catch (Exception e) {
+            throw new Exception("Erro Eliminar Trabajador " +e.getMessage());
+        }
+    }
+    
+    public void update(Trabajador trabajador) throws Exception {
+        try {
+            em.merge(trabajador);
+        } catch (Exception e) {
+            throw new Exception("Erro actualizar Trabajador " +e.getMessage());
+        }
+    }
+
+    public Trabajador read(int id) throws Exception {
+        try {
+            return em.find(Trabajador.class, id);
+        } catch (Exception e) {
+            throw new Exception("Erro leer Trabajador " +e.getMessage());
+        }
+    }
+
+    public List<Trabajador> findAll() throws Exception {
+
+        try {
+            Query q = em.createNamedQuery("Trabajador.findAll");
+            List<Trabajador> lista = q.getResultList();
+            return lista;
+        } catch (Exception e) {
+            throw new Exception("Erro listar Trabajador " +e.getMessage());
+        }
+
+    }
+
+    public Trabajador findByCedula(String cedula) throws Exception {
+        try {
+            String jpql = "SELECT P FROM Trabajador p "
+                    + "WHERE cedula = :cedula";
+            Query q = em.createQuery(jpql, Trabajador.class);
+            q.setParameter("cedula", cedula);
+
+            return (Trabajador) q.getSingleResult();
+        } catch (Exception e) {
+            throw new Exception("Erro buscar por  cedula");
+        }
+
+    }
+
+    public int maxId() throws Exception {
+        try {
+            String jpql = "SELECT P FROM Trabajador p "
+                    + "WHERE cedula = :cedula";
+            Query q = em.createQuery(jpql, Trabajador.class);
+            return (int) q.getSingleResult();
+        } catch (Exception e) {
+            throw new Exception("Error MaxID", e.getCause());
+        }
+    }
+
+}
