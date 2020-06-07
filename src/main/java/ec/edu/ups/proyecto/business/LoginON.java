f953250f311973be445a9b7dea024a0cdd40ebfb
Ricardo Vinicio Jara jara <5940607-RicardoVinicioJara@users.noreply.gitlab.com>
2020-06-07 05:38:50 -0500

    Auditoria de login

--- src/main/java/ec/edu/ups/proyecto/business/LoginON.java
+++ src/main/java/ec/edu/ups/proyecto/business/LoginON.java
@@ -5,10 +5,15 @@
  */
 package ec.edu.ups.proyecto.business;
 
+import ec.edu.ups.proyecto.dao.AloginDAO;
 import ec.edu.ups.proyecto.dao.ClienteDAO;
 import ec.edu.ups.proyecto.dao.TrabajadorDAO;
+import ec.edu.ups.proyecto.emtitis.Alogin;
 import ec.edu.ups.proyecto.emtitis.Cliente;
 import ec.edu.ups.proyecto.emtitis.Trabajador;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
@@ -18,76 +23,118 @@ import javax.inject.Inject;
  *
  * @author fanny
  */
-
 @Stateless
 public class LoginON {
-    
+
+    @Inject
+    AloginDAO aloginDAO;
+
     @Inject
     TrabajadorDAO trabajadorDAO;
-    
+
     @Inject
     ClienteDAO clienteDAO;
-    
-    
-    public Trabajador loginTra(String cedula, String contrasenia){
+
+    public Trabajador loginTra(String cedula, String contrasenia) {
         try {
-            Trabajador trabajador= trabajadorDAO.findByCedula(cedula);
-            if(trabajador!=null){
-                
-                if(trabajador.getContracenia().equals(contrasenia)){
-                    
-                    //Enviar correo de login con ip
+            Trabajador trabajador = trabajadorDAO.findByCedula(cedula);
+            if (trabajador != null) {
+
+                if (trabajador.getContracenia().equals(contrasenia)) {
+                    enviarCorreoIngreso(trabajador.getCorreo(), true, obtenerIp());
+                    //Aditoria de login de trabajdor >> true
                     return trabajador;
-                }else{
-                    //enviar correo de login erroneo, alguien intenta usar su cedula para entrar
+                } else {
+                    //Aditoria de login de trabajdor >> false
+                    enviarCorreoIngreso(trabajador.getCorreo(), true, obtenerIp());
                 }
             }
         } catch (Exception ex) {
             Logger.getLogger(LoginON.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return null;
-        
+
     }
-    
-    public Cliente loginClie(String cedula, String contrasenia){
+
+    public Cliente loginClie(String cedula, String contrasenia) {
         try {
-            Cliente cliente= clienteDAO.findByCedula(cedula);
-            if(cliente!=null){
-                
-                if(cliente.getContracenia().equals(contrasenia)){
-                    
-                    //Enviar correo de login con ip
+            Cliente cliente = clienteDAO.findByCedula(cedula);
+            if (cliente != null) {
+
+                if (cliente.getContracenia().equals(contrasenia)) {
+                    guardarLogin(cliente, true);
+                    enviarCorreoIngreso(cliente.getCorreo(), true, obtenerIp());
                     return cliente;
-                }else{
-                    //enviar correo de login erroneo, alguien intenta usar su cedula para entrar
+                } else {
+                    guardarLogin(cliente, false);
+                    enviarCorreoIngreso(cliente.getCorreo(), false, obtenerIp());
                 }
             }
         } catch (Exception ex) {
             Logger.getLogger(LoginON.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return null;
-        
+
     }
-    
-    public void actualizarTrabajador(Trabajador trabajador){
+
+    public void actualizarTrabajador(Trabajador trabajador) {
         try {
             trabajadorDAO.update(trabajador);
-            //Menseje de que la contra cambiada
+            enviarCorreoUpdate(trabajador.getCorreo());
         } catch (Exception ex) {
             Logger.getLogger(TrabajadorON.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void actualizarCliente(Cliente cliente){
+
+    public void actualizarCliente(Cliente cliente) {
         try {
             clienteDAO.update(cliente);
+            enviarCorreoUpdate(cliente.getCorreo());
         } catch (Exception ex) {
             Logger.getLogger(ClienteON.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    public void guardarLogin(Cliente cliente, boolean acceso) {
+        Alogin login = new Alogin();
+        try {
+            InetAddress ip;
+            login.setAcceso(acceso);
+            login.setCliente(cliente);
+            login.setFecha(new Date(new Date().getYear(), new Date().getMonth(), new Date().getDay()));
+            login.setIp(obtenerIp());
+            aloginDAO.insert(login);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            Logger.getLogger(ClienteON.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
     
+    public String obtenerIp() {
+        InetAddress ip;
+        try {
+            ip = InetAddress.getLocalHost();
+            return ip.getHostName();
+        } catch (UnknownHostException e) {
+            System.out.println("Error: " + e.getMessage());
+            Logger.getLogger(ClienteON.class.getName()).log(Level.SEVERE, null, e);
+            return "";
+        }
+    }
     
+    public void enviarCorreoIngreso( String correo, boolean acceso, String ip) {
+        //El correo viene en la variable correo
+        //(acceso = true ) >> enviar el correo con login correcto si es acceso es true
+        //(acceso = false ) >> enviar el correo con: se registra un intento erroneo de acceso con su cuenta 
+        // Ip >> lugar desde donde intentan usar
+    }
     
+    public void enviarCorreoUpdate(String correo){
+        //enviar un correo diciendo se a autualizado la contra
+        //No importa si es cliente o si es trabajdor
+    }
+    
+
 }
--- src/main/java/ec/edu/ups/proyecto/emtitis/Alogin.java
+++ src/main/java/ec/edu/ups/proyecto/emtitis/Alogin.java
@@ -55,7 +55,7 @@ public class Alogin implements Serializable {
     private String ip;
     @Basic(optional = false)
     @Column(name = "acceso")
-    private short acceso;
+    private boolean acceso;
     @JoinColumn(name = "cliente", referencedColumnName = "id")
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Cliente cliente;
@@ -67,7 +67,7 @@ public class Alogin implements Serializable {
         this.id = id;
     }
 
-    public Alogin(Integer id, Date fecha, String ip, short acceso) {
+    public Alogin(Integer id, Date fecha, String ip, boolean acceso) {
         this.id = id;
         this.fecha = fecha;
         this.ip = ip;
@@ -98,11 +98,11 @@ public class Alogin implements Serializable {
         this.ip = ip;
     }
 
-    public short getAcceso() {
+    public boolean getAcceso() {
         return acceso;
     }
 
-    public void setAcceso(short acceso) {
+    public void setAcceso(boolean acceso) {
         this.acceso = acceso;
     }
 
