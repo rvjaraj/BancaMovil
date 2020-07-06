a107a0de380cd9b53523c95f8cd267cac89626b3
JuanC <49213049+jcuyaguari@users.noreply.github.com>
2020-07-06 16:31:59 -0500

    crear entidad errores

---
+++ src/main/java/ec/edu/ups/proyecto/emtitis/Errores.java
@@ -0,0 +1,29 @@
+package ec.edu.ups.proyecto.emtitis;
+
+public class Errores {
+
+	private int codigo;
+	private String nombre;
+	private String descripcion;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+}
