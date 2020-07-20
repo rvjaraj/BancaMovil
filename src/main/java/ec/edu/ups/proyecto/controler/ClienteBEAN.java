481982dc725d5ee58ff8ab540eeee12aca9da095
Ricardo Vinicio Jara jara <5940607-RicardoVinicioJara@users.noreply.gitlab.com>
2020-07-19 23:23:10 -0500

    registro de  cliente
    - Agrego para que el cliente  tenga los atriutos fecha de nacimiento y estado civil

--- src/main/java/ec/edu/ups/proyecto/controler/ClienteBEAN.java
+++ src/main/java/ec/edu/ups/proyecto/controler/ClienteBEAN.java
@@ -8,15 +8,12 @@ package ec.edu.ups.proyecto.controler;
 import ec.edu.ups.proyecto.business.ClienteON;
 import ec.edu.ups.proyecto.business.ResumenCuentaON;
 import ec.edu.ups.proyecto.emtitis.Cliente;
-import ec.edu.ups.proyecto.emtitis.Cuenta;
 import ec.edu.ups.proyecto.emtitis.Transaciones;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -37,26 +34,23 @@ public class ClienteBEAN {
     private Cliente newCliente;
     private Cliente auxCliente;
     private List<Cliente> listaClientees;
-    private ArrayList<String> listaOpc;
     private String textoBuscar;
     private String cuenta;
     private String cedulaPersona;
-    private List<Transaciones> listaCuentasClientes =  new ArrayList<Transaciones>();
-    private List<Cliente> listaClientes =  new ArrayList<Cliente>();
+    private List<Transaciones> listaCuentasClientes = new ArrayList<Transaciones>();
+    private List<Cliente> listaClientes = new ArrayList<Cliente>();
     private List<Transaciones> listaEstadosCta = new ArrayList<Transaciones>();
     private Date fechaInicio;
     private Date fechaFin;
     private String tipo;
-    private Transaciones newTransaciones = new Transaciones();
-    
+    private List<String> estadocivilist;
+
     public ClienteBEAN() {
     }
-    
-    
 
     @Inject
     private ClienteON clienteON;
-    
+
     @Inject
     private ResumenCuentaON resumenCuentaON;
 
@@ -66,33 +60,44 @@ public class ClienteBEAN {
         cuenta = clienteON.numeroCuenta();
         listaClientees = clienteON.listaClientees();
         textoBuscar = "";
-//        fechaInicio = null;
-//        fechaFin = null;
-//        tipo = "";
+        estadocivilist = new ArrayList<>();
+        estadocivilist.add("Divorciado");
+        estadocivilist.add("Divorciada");
+        estadocivilist.add("Soltero");
+        estadocivilist.add("Soltera");
+        estadocivilist.add("Casado");
+        estadocivilist.add("Casada");
+
         auxCliente = new Cliente();
         loadResumenCliente();
-        
+
         action();
     }
+
     /**
-     * Permite obtener el dato que se pasa a traves de la url para
-     * poder manejar en los distintos metodos.
+     * Permite obtener el dato que se pasa a traves de la url para poder manejar
+     * en los distintos metodos.
      */
     public void action() {
         HttpServletRequest request = (HttpServletRequest) FacesContext
                 .getCurrentInstance().getExternalContext().getRequest();
         String param = request.getParameter("tipo");
         tipo = param;
-        
+
     }
+
     /**
-     * Guarda el cliente desde la vista consumiendo 
-     * la logica de negocio existente.
+     * Guarda el cliente desde la vista consumiendo la logica de negocio
+     * existente.
+     *
      * @return
      */
     public String guardarCliente() {
+        System.out.println("aqui");
+        System.out.println(newCliente.getFechNac().toString());
+        System.out.println("hire");
         try {
-            System.out.println(newCliente.toString()+">>>>");
+            System.out.println(newCliente.toString() + ">>>>");
             clienteON.guardarCliente(newCliente, cuenta);
             init();
         } catch (Exception ex) {
@@ -100,9 +105,11 @@ public class ClienteBEAN {
         }
         return null;
     }
+
     /**
-     * Busca los clientes por el codigo que se le pase como parametro desde
-     * la vista y se pasa al objeto de negocio.
+     * Busca los clientes por el codigo que se le pase como parametro desde la
+     * vista y se pasa al objeto de negocio.
+     *
      * @return
      */
     public String buscaClientees() {
@@ -114,37 +121,42 @@ public class ClienteBEAN {
         }
         return null;
     }
+
     /**
-     * Busca a los clientes por id y pasa el parametro a 
-     * los objetos de negocio 
+     * Busca a los clientes por id y pasa el parametro a los objetos de negocio
+     *
      * @param id
      * @return
      */
     public String buscaClienteID(String id) {
         try {
-             auxCliente = clienteON.buscarCliente(id);
+            auxCliente = clienteON.buscarCliente(id);
         } catch (Exception ex) {
             Logger.getLogger(ClienteBEAN.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
-    
+
     /**
      * Actualiza el cliente hace uso de la logica de negocio
+     *
      * @return
      */
-    public String actualizarCliente(){
+    public String actualizarCliente() {
         clienteON.actualizarCliente(auxCliente);
         init();
         System.out.println("actualizado");
         return null;
     }
-    /***
-     * Elimina el cliente, recibe al objeto
-     * cliente para poder actualizar los datos. 
+
+    /**
+     * *
+     * Elimina el cliente, recibe al objeto cliente para poder actualizar los
+     * datos.
+     *
      * @return
      */
-    public String eliminarCliente(){
+    public String eliminarCliente() {
         auxCliente.setEliminado(true);
         auxCliente.getCuentaList().get(0).setEliminado(true);
         clienteON.actualizarCliente(auxCliente);
@@ -152,45 +164,48 @@ public class ClienteBEAN {
         System.out.println("Eliminado");
         return null;
     }
-    
+
     /**
-     * Metodo de prueba que carga el resumen del cliente, 
-     * buscado por la cedula, imprime lo que retorna.
+     * Metodo de prueba que carga el resumen del cliente, buscado por la cedula,
+     * imprime lo que retorna.
+     *
      * @return
      */
     public String loadResumenCliente() {
-    	listaClientes = resumenCuentaON.getClienteByCedelua("0106");
-    	for (Cliente cliente : listaClientes) {
-    		loadResumenCuenta(cliente.getCuentaList().get(0).getNumero());
-		}
-    	return null;
+        listaClientes = resumenCuentaON.getClienteByCedelua("0106");
+        for (Cliente cliente : listaClientes) {
+            loadResumenCuenta(cliente.getCuentaList().get(0).getNumero());
+        }
+        return null;
     }
+
     /**
-     * Metodo de prueba que carga el resumen del cliente
-     * recibe como parametro al numero
+     * Metodo de prueba que carga el resumen del cliente recibe como parametro
+     * al numero
+     *
      * @param numero
      * @return
      */
     public String loadResumenCuenta(String numero) {
-    	listaCuentasClientes = resumenCuentaON.getResumenCuentaCliente(numero);
-    	return null;
+        listaCuentasClientes = resumenCuentaON.getResumenCuentaCliente(numero);
+        return null;
     }
-    
+
     /**
-     * Metodo de prueba que carga el resumen del cliente
-     * Carga el estado de la cuenta 
+     * Metodo de prueba que carga el resumen del cliente Carga el estado de la
+     * cuenta
+     *
      * @return
      */
     public String loadEstadoCta() {
-    	listaEstadosCta = resumenCuentaON.getEstadoCtaByMes(tipo, fechaInicio, fechaFin);
-    	System.out.println("llega los paametros >>>>> "+ tipo +" fehcIni: "+ fechaInicio+ " fechaFin: "+ fechaFin);
-    	for (Transaciones t : listaEstadosCta) {
-			System.out.println("fechas--"+t.getFecha());
-		}
-    	return null;
+        listaEstadosCta = resumenCuentaON.getEstadoCtaByMes(tipo, fechaInicio, fechaFin);
+        System.out.println("llega los paametros >>>>> " + tipo + " fehcIni: " + fechaInicio + " fechaFin: " + fechaFin);
+        for (Transaciones t : listaEstadosCta) {
+            System.out.println("fechas--" + t.getFecha());
+        }
+        return null;
     }
-    
-    
+
     // -------------------> 
     public Cliente getNewCliente() {
         return newCliente;
@@ -206,14 +221,6 @@ public class ClienteBEAN {
 
     public void setClienteON(ClienteON clienteON) {
         this.clienteON = clienteON;
-    }
-
-    public ArrayList<String> getListaOpc() {
-        return listaOpc;
-    }
-
-    public void setListaOpc(ArrayList<String> listaOpc) {
-        this.listaOpc = listaOpc;
     }
 
     public String getTextoBuscar() {
@@ -244,68 +251,72 @@ public class ClienteBEAN {
         return cuenta;
     }
 
+    public List<String> getEstadocivilist() {
+        return estadocivilist;
+    }
+
+    public void setEstadocivilist(List<String> estadocivilist) {
+        this.estadocivilist = estadocivilist;
+    }
+
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
-	public String getCedulaPersona() {
-		return cedulaPersona;
-	}
+    public String getCedulaPersona() {
+        return cedulaPersona;
+    }
 
-	public void setCedulaPersona(String cedulaPersona) {
-		this.cedulaPersona = cedulaPersona;
-	}
+    public void setCedulaPersona(String cedulaPersona) {
+        this.cedulaPersona = cedulaPersona;
+    }
 
-	public List<Transaciones> getListaCuentasClientes() {
-		return listaCuentasClientes;
-	}
+    public List<Transaciones> getListaCuentasClientes() {
+        return listaCuentasClientes;
+    }
 
-	public void setListaCuentasClientes(List<Transaciones> listaCuentasClientes) {
-		this.listaCuentasClientes = listaCuentasClientes;
-	}
+    public void setListaCuentasClientes(List<Transaciones> listaCuentasClientes) {
+        this.listaCuentasClientes = listaCuentasClientes;
+    }
 
-	public List<Cliente> getListaClientes() {
-		return listaClientes;
-	}
+    public List<Cliente> getListaClientes() {
+        return listaClientes;
+    }
 
-	public void setListaClientes(List<Cliente> listaClientes) {
-		this.listaClientes = listaClientes;
-	}
+    public void setListaClientes(List<Cliente> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
 
-	public List<Transaciones> getListaEstadosCta() {
-		return listaEstadosCta;
-	}
+    public List<Transaciones> getListaEstadosCta() {
+        return listaEstadosCta;
+    }
 
-	public void setListaEstadosCta(List<Transaciones> listaEstadosCta) {
-		this.listaEstadosCta = listaEstadosCta;
-	}
+    public void setListaEstadosCta(List<Transaciones> listaEstadosCta) {
+        this.listaEstadosCta = listaEstadosCta;
+    }
 
-	public Date getFechaInicio() {
-		return fechaInicio;
-	}
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
 
-	public void setFechaInicio(Date fechaInicio) {
-		this.fechaInicio = fechaInicio;
-	}
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
 
-	public Date getFechaFin() {
-		return fechaFin;
-	}
+    public Date getFechaFin() {
+        return fechaFin;
+    }
 
-	public void setFechaFin(Date fechaFin) {
-		this.fechaFin = fechaFin;
-	}
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
 
-	public String getTipo() {
-		return tipo;
-	}
+    public String getTipo() {
+        return tipo;
+    }
 
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-
-	
-	
-	
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 
 }
