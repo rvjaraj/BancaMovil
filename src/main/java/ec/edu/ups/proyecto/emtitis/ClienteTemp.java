30f57db3f181fffca7623266a877a8b67d67acaa
JuanC <49213049+jcuyaguari@users.noreply.github.com>
2020-08-05 16:21:44 -0500

    ServicioMovil

---
+++ src/main/java/ec/edu/ups/proyecto/emtitis/ClienteTemp.java
@@ -0,0 +1,26 @@
+package ec.edu.ups.proyecto.emtitis;
+
+public class ClienteTemp {
+
+	String cedula; 
+	String contrasenia;
+	
+	public ClienteTemp() {
+		
+	}
+	
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+	
+	
+}
