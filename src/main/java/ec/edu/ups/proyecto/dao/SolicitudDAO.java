5f15be3cd197a1befcf80cae1a2ea7efe4ef6758
FannyCristina <fanny-crisss@hotmail.es>
2020-08-10 14:18:47 -0500

    delete

--- src/main/java/ec/edu/ups/proyecto/dao/SolicitudDAO.java
+++ src/main/java/ec/edu/ups/proyecto/dao/SolicitudDAO.java
@@ -12,71 +12,77 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
-
 @Stateless
 public class SolicitudDAO {
 
-    @PersistenceContext(name="BancaMovilPersistenceUnit")
+    @PersistenceContext(name = "BancaMovilPersistenceUnit")
     private EntityManager em;
 
     public SolicitudDAO() {
     }
-     
-   /**
-    * Este metodo permite guadar la solicutud mediante el parametro solicitud
-    * @param solicitud
-    * @return
-    * @throws Exception 
-    */ 
+
+    /**
+     * Este metodo permite guadar la solicutud mediante el parametro solicitud
+     *
+     * @param solicitud
+     * @return
+     * @throws Exception
+     */
     public boolean insert(Solicitud solicitud) throws Exception {
         boolean bandera = true;
-    	try {
+        try {
             em.persist(solicitud);
-            bandera=true;
+            bandera = true;
         } catch (Exception e) {
-        	bandera=false;
+            bandera = false;
             throw new Exception("Erro ingreso Solicitud " + e.getMessage());
         }
-        
+
         return bandera;
     }
-     
+
+    /**
+     * Este metodo permite eliminar la solicitu
+     *
+     * @param solicitud
+     * @throws Exception
+     */
+
     public void delete(Solicitud solicitud) throws Exception {
         try {
             System.out.println("borrando");
             em.remove(read(solicitud.getId()));
         } catch (Exception e) {
-            throw new Exception("oErro Eliminar Solicitud " +e.getMessage());
+            throw new Exception("oErro Eliminar Solicitud " + e.getMessage());
         }
     }
-        
 
     public void deleteId(int id) throws Exception {
         try {
             System.out.println("borrando");
             em.remove(read(id));
         } catch (Exception e) {
-            throw new Exception("oErro Eliminar Solicitud " +e.getMessage());
+            throw new Exception("oErro Eliminar Solicitud " + e.getMessage());
         }
     }
-       
+
     public void update(Solicitud solicitud) throws Exception {
         try {
             em.merge(solicitud);
         } catch (Exception e) {
-            throw new Exception("Erro actualizar Solicitud " +e.getMessage());
+            throw new Exception("Erro actualizar Solicitud " + e.getMessage());
         }
     }
-    
+
     public Solicitud read(int id) throws Exception {
         try {
             System.out.println("Estamos aca");
             return em.find(Solicitud.class, id);
         } catch (Exception e) {
-            throw new Exception("Erro leer Solicitud " +e.getMessage());
+            throw new Exception("Erro leer Solicitud " + e.getMessage());
         }
     }
-   
+
     public List<Solicitud> findAll() throws Exception {
 
         try {
@@ -84,18 +90,18 @@ public class SolicitudDAO {
             List<Solicitud> lista = q.getResultList();
             return lista;
         } catch (Exception e) {
-            throw new Exception("Erro listar Solicitud " +e.getMessage());
+            throw new Exception("Erro listar Solicitud " + e.getMessage());
         }
 
     }
-    
-     public List<Solicitud>  findByCliente(String cedula ) throws Exception {
+
+    public List<Solicitud> findByCliente(String cedula) throws Exception {
         try {
             Query q = em.createNamedQuery("Solicitud.findByCliente");
             q.setParameter("cedula", cedula);
-            return  q.getResultList();
+            return q.getResultList();
         } catch (Exception e) {
-            throw new Exception("Erro buscar por  cedula clieente " +e.getMessage());
+            throw new Exception("Erro buscar por  cedula clieente " + e.getMessage());
         }
     }
 
@@ -105,11 +111,9 @@ public class SolicitudDAO {
             q.setParameter("id", Integer.parseInt(id));
             return (Solicitud) q.getSingleResult();
         } catch (Exception e) {
-            throw new Exception("Erro buscar por  ID " +e.getMessage());
+            throw new Exception("Erro buscar por  ID " + e.getMessage());
         }
 
     }
-    
- 
 
 }
